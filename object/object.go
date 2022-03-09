3964e31df29b2be6a109a5f86575fa2a930198b0
Ris Adams <chris@tuesdaybegins.com>
2022-03-09 08:58:37 -0500

    added Evaluator

--- object/object.go
+++ object/object.go
@@ -1,32 +1,31 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"monkey/ast"
+	"strings"
 )
 
 type ObjectType string
 
 const (
-	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ  = "NULL"
+	ERROR_OBJ = "ERROR"
+
 	INTEGER_OBJ = "INTEGER"
-	NULL_OBJ    = "NULL"
+	BOOLEAN_OBJ = "BOOLEAN"
+
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+
+	FUNCTION_OBJ = "FUNCTION"
 )
 
-// the base from which all other objects are derived
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
 
-// Boolean, represents a true or false value
-type Boolean struct {
-	Value bool
-}
-
-func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
-
-// Integer, represents a discrete 64 bit number
 type Integer struct {
 	Value int64
 }
@@ -34,8 +33,53 @@ type Integer struct {
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
-// represents an object that is null (abscent of value)
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
 type Null struct{}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
